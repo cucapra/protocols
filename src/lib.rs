873ae94edcea0ca9f39857611836e47913bb820a
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2024-12-05 15:44:39 -0500

    Seperate files into different tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,22 @@ mod tests {
     }
 
     #[test]
-    fn it_works() {
+    fn test_add_prot() {
         parse_file("tests/add.prot");
+    }
+
+    #[test]
+    fn test_calyx_go_done_prot() {
         parse_file("tests/calyx_go_done.prot");
+    }
+
+    #[test]
+    fn test_mul_prot() {
         parse_file("tests/mul.prot");
+    }
+
+    #[test]
+    fn test_cond_prot() {
         parse_file("tests/cond.prot");
     }
 }
