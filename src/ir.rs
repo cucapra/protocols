6f2c7e0abac20823e5d697d355b30ba8670deb3a
Kevin Läufer <laeufer@cornell.edu>
2024-12-10 15:21:00 -0500

    try to use patronus

--- src/ir.rs
+++ src/ir.rs
@@ -3,43 +3,98 @@
 // author: Nikil Shyamunder <nikil.shyamsunder@gmail.com>
 // author: Kevin Laeufer <laeufer@cornell.edu>
 
-use cranelift_entity::*;
+use patronus::expr::{Context, Expr, ExprRef, StringRef, WidthInt};
+use rustc_hash::FxHashMap;
+use std::borrow::Cow;
 use std::ops::Index;
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Transaction {
-    name: String,
-    stmts: PrimaryMap<StmtId, Stmt>,
-    exprs: PrimaryMap<ExprId, Expr>,
+    name: StringRef,
+    args: Vec<Arg>,
+    body: Vec<Stmt>,
 }
 
-impl Index<StmtId> for Transaction {
-    type Output = Stmt;
-
-    fn index(&self, index: StmtId) -> &Self::Output {
-        &self.stmts[index]
-    }
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Arg {
+    dir: Dir,
+    name: StringRef,
+    tpe: Type,
 }
 
-impl Index<ExprId> for Transaction {
-    type Output = Expr;
-
-    fn index(&self, index: ExprId) -> &Self::Output {
-        &self.exprs[index]
-    }
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Dir {
+    In,
+    Out,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct StmtId(u32);
-entity_impl!(StmtId, "stmt");
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Type {
+    BitVec(WidthInt),
+    /// Placeholder type for `dut`
+    Dut,
+    /// Type taken on when we do not know the actual type yet
+    Unknown,
+}
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Stmt {
-    Assign(ExprId, ExprId),
+    Assign(ExprRef, ExprRef),
     Step,
     Fork,
-    While(ExprId),
+    While(ExprRef, Vec<Stmt>),
+    IfElse(ExprRef, Vec<Stmt>, Vec<Stmt>),
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct ExprId(u32);
-entity_impl!(ExprId, "expr");
-pub enum Expr {}
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct SymbolTable {
+    entries: Vec<SymbolTableEntry>,
+    by_name: FxHashMap<String, u32>,
+    by_expr: FxHashMap<ExprRef, u32>,
+}
+
+impl SymbolTable {
+    pub fn declare(&mut self, name: Cow<str>, tpe: Type) -> ExprRef {
+        assert!(
+            !self.by_name.contains_key(name.as_ref()),
+            "we already have an entry for {}!",
+            name.as_ref()
+        );
+    }
+}
+
+impl Index<&str> for SymbolTable {
+    type Output = SymbolTableEntry;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        let index = self.by_name[index];
+        &self.entries[index as usize]
+    }
+}
+
+impl Index<ExprRef> for SymbolTable {
+    type Output = SymbolTableEntry;
+
+    fn index(&self, index: ExprRef) -> &Self::Output {
+        let index = self.by_expr[index];
+        &self.entries[index as usize]
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct SymbolTableEntry {
+    name: StringRef,
+    tpe: Type,
+    parent: ExprRef,
+    next: ExprRef,
+}
+
+impl SymbolTableEntry {
+    pub fn name(&self, ctx: &Context) -> &str {
+        &ctx[self.name]
+    }
+
+    pub fn tpe(&self) -> Type {
+        self.tpe.clone()
+    }
+}
