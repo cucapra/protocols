906ebc6fe04b383dbc5a79c39d15e74febf96760
fpham0701 <fpham0701@gmail.com>
2025-03-13 13:21:51 -0400

    cargo fmt

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,12 +1,11 @@
+use crate::ir::*;
 use baa::BitVecValue;
 use marlin_verilator::*;
-use patronus::{sim::{Interpreter, Simulator}};
-use crate::ir::*;
+use patronus::sim::{Interpreter, Simulator};
 
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 fn mapping(tr: &Transaction, st: &SymbolTable, stmtid: &StmtId, sim: &mut Interpreter) {
-
     match &tr[stmtid] {
         Stmt::Skip => sim.step(),
         Stmt::Block(stmts) => {
@@ -18,17 +17,21 @@ fn mapping(tr: &Transaction, st: &SymbolTable, stmtid: &StmtId, sim: &mut Interp
         Stmt::Step(_) => sim.step(),
         Stmt::Fork => todo!(),
         Stmt::While(_, _) => todo!(),
-        Stmt::IfElse(_, _, _ ) => todo!(),
-        Stmt::AssertEq(_,_ ) => todo!(),
+        Stmt::IfElse(_, _, _) => todo!(),
+        Stmt::AssertEq(_, _) => todo!(),
     }
 }
-
 
 // Need to simulate protocol given a verilog file of a struct
 // When given inputs, map it to expr ref
 
-
-pub fn interpret(btor_path: &str, in_vals: HashMap<&str, u64>, out: (&str, u64), tr: &Transaction, st: &SymbolTable) -> bool {
+pub fn interpret(
+    btor_path: &str,
+    in_vals: HashMap<&str, u64>,
+    out: (&str, u64),
+    tr: &Transaction,
+    st: &SymbolTable,
+) -> bool {
     let (ctx, sys) = match patronus::btor2::parse_file(btor_path) {
         Some(result) => result,
         None => {
@@ -41,7 +44,11 @@ pub fn interpret(btor_path: &str, in_vals: HashMap<&str, u64>, out: (&str, u64),
 
     let mut inputs = HashMap::new();
     for (name, val) in in_vals {
-        let var = *sys.inputs.iter().find(|i| ctx.get_symbol_name(**i).unwrap() == name).unwrap();
+        let var = *sys
+            .inputs
+            .iter()
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == name)
+            .unwrap();
         inputs.insert(var, val);
     }
 
@@ -61,8 +68,6 @@ pub fn interpret(btor_path: &str, in_vals: HashMap<&str, u64>, out: (&str, u64),
 
     true
 }
-
-
 
 #[cfg(test)]
 pub mod tests {
@@ -130,13 +135,12 @@ pub mod tests {
             .unwrap();
     }
 
-
     #[test]
     fn run_interpret() {
         let mut inputs = HashMap::new();
         inputs.insert("A", 6);
         inputs.insert("B", 7);
-        
+
         let mut outputs = HashMap::new();
         outputs.insert("S", 13);
         //let success = interpret("examples/adders/add_d1.btor", inputs, outputs);
