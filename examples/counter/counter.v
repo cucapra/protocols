fc6b57c41c6f61d7953cc01b07215b807bf59717
francis <fpham0701@gmail.com>
2025-03-24 10:59:43 -0400

    Update Yosys, Added Counter Test
    - updated yosys to previous implementation
    - Added Counter Test that fully takes in verilog file and converts to btor and then simulates it using patronus
    - Full backend stretch

--- examples/counter/counter.v
+++ examples/counter/counter.v
@@ -1,13 +1,13 @@
 module counter(
     input clk,        
-    output reg [6:0] count
+    output reg [6:0] c
 );
 
     always @(posedge clk) begin
-        if (count == 100) begin
-            count <= 7'b0; 
+        if (c == 10) begin
+            c <= 7'b0; 
         end else begin
-            count <= count + 1;
+            c <= c + 1;
         end
     end
 endmodule
--- src/interface.rs
+++ src/interface.rs
@@ -1,12 +1,19 @@
+// Copyright 2024 Cornell University
+// released under MIT License
+// author: Nikil Shyamunder <nikil.shyamsunder@gmail.com>
+// author: Kevin Laeufer <laeufer@cornell.edu>
+// author: Francis Pham <fdp25@cornell.edu>
+
 use crate::ir::*;
 use marlin_verilator::*;
-use patronus::sim::{Interpreter, Simulator};
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use crate::yosys::*;
     use baa::{BitVecOps, BitVecValue};
     use patronus::sim::Simulator;
+    use std::path::PathBuf;
 
     /// This example is intended to demonstrate how the `verilog` crate can be used
     /// to execute a design with Verilator. This is - of course - not a working
@@ -98,35 +105,134 @@ pub mod tests {
     }
 
     #[test]
-    fn run_add_d1_with_patronus() {
-        let (ctx, sys) = patronus::btor2::parse_file("examples/adders/add_d1.btor").unwrap();
+    fn run_add_d0_with_patronus() {
+        let (ctx, sys) = patronus::btor2::parse_file("examples/adders/add_d0.btor").unwrap();
         let mut sim = patronus::sim::Interpreter::new(&ctx, &sys);
-
-        for name in sys.inputs.iter() {
-            println!("{:#?}", name);
-        }
 
         let a = *sys
             .inputs
             .iter()
-            .find(|i| ctx.get_symbol_name(**i).unwrap() == "A")
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "a")
             .unwrap();
         let b = *sys
             .inputs
             .iter()
-            .find(|i| ctx.get_symbol_name(**i).unwrap() == "B")
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "b")
             .unwrap();
         let s = sys
             .outputs
             .iter()
-            .find(|o| ctx[o.name] == "S")
+            .find(|o| ctx[o.name] == "s")
             .unwrap()
             .expr;
 
         sim.init();
-        sim.set(a, &BitVecValue::from_u64(6, 32));
-        sim.set(b, &BitVecValue::from_u64(7, 32));
+        sim.set(a, &BitVecValue::from_u64(41, 32));
+        sim.set(b, &BitVecValue::from_u64(59, 32));
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 100);
         sim.step();
-        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 13);
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 100); // First step should keep value
+    }
+
+    #[test]
+    fn run_add_d1_with_patronus() {
+        let (ctx, sys) = patronus::btor2::parse_file("examples/adders/add_d1.btor").unwrap();
+        let mut sim = patronus::sim::Interpreter::new(&ctx, &sys);
+
+        let a = *sys
+            .inputs
+            .iter()
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "a")
+            .unwrap();
+        let b = *sys
+            .inputs
+            .iter()
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "b")
+            .unwrap();
+        let s = sys
+            .outputs
+            .iter()
+            .find(|o| ctx[o.name] == "s")
+            .unwrap()
+            .expr;
+
+        sim.init();
+        sim.set(a, &BitVecValue::from_u64(4, 32));
+        sim.set(b, &BitVecValue::from_u64(6, 32));
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 0);
+        sim.step();
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 10); // First step should have value
+    }
+
+    #[test]
+    fn run_add_d2_with_patronus() {
+        let (ctx, sys) = patronus::btor2::parse_file("examples/adders/add_d2.btor").unwrap();
+        let mut sim = patronus::sim::Interpreter::new(&ctx, &sys);
+
+        let a = *sys
+            .inputs
+            .iter()
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "a")
+            .unwrap();
+        let b = *sys
+            .inputs
+            .iter()
+            .find(|i| ctx.get_symbol_name(**i).unwrap() == "b")
+            .unwrap();
+        let s = sys
+            .outputs
+            .iter()
+            .find(|o| ctx[o.name] == "s")
+            .unwrap()
+            .expr;
+
+        sim.init();
+        sim.set(a, &BitVecValue::from_u64(10, 32));
+        sim.set(b, &BitVecValue::from_u64(5, 32));
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 0); // No step should be nothing
+        sim.step();
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 0); // First step should be nothing
+        sim.step();
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 15); // Register should now have value
+        sim.step();
+        assert_eq!(sim.get(s).unwrap().to_u64().unwrap(), 15); // Register should keep value
+    }
+
+    #[test]
+    fn run_counter_with_patronus() {
+        let env = YosysEnv::default();
+        let inp = PathBuf::from("examples/counter/counter.v");
+        let mut proj = ProjectConf::with_source(inp);
+        let btor_file = yosys_to_btor(&env, &mut proj, None).unwrap();
+
+        let (ctx, sys) = patronus::btor2::parse_file(btor_file.as_path().as_os_str()).unwrap();
+        let mut sim = patronus::sim::Interpreter::new(&ctx, &sys);
+
+        let c = sys
+            .outputs
+            .iter()
+            .find(|o| ctx[o.name] == "c")
+            .unwrap()
+            .expr;
+
+        sim.init();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 0);
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 1);
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 2);
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 3);
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 4);
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 5);
+        sim.step();
+        sim.step();
+        sim.step();
+        sim.step();
+        sim.step();
+        sim.step();
+        assert_eq!(sim.get(c).unwrap().to_u64().unwrap(), 0); // Reseting to 0
     }
 }
