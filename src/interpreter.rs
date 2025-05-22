8591b71edce0247bc30f115e3a631c0dabf62d10
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2025-05-22 18:37:43 -0400

    fix

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -329,7 +329,7 @@ impl<'a> Evaluator<'a> {
             }
             Stmt::Block(stmt_ids) => {
                 if stmt_ids.is_empty() {
-                    Ok(None)
+                    return Ok(self.next_stmt_map[stmt_id]);
                 } else {
                     Ok(Some(stmt_ids[0]))
                 }
