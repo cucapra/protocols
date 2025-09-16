648305e5adcce9fa8c69f737a88633b8e35e750a
Ernest Ng <eyn5@cornell.edu>
2025-09-16 17:47:29 -0400

    Port Counter test to Turnt

--- protocols/src/scheduler.rs
+++ protocols/src/scheduler.rs
@@ -695,34 +695,6 @@ pub mod tests {
     }
 
     #[test]
-    fn test_scheduler_counter() {
-        let handler = &mut DiagnosticHandler::default();
-        let (parsed_data, ctx, sys) = setup_test_environment(
-            vec!["tests/counters/counter.v"],
-            "tests/counters/counter.prot",
-            None,
-            handler,
-        );
-
-        let transactions_and_symbols: Vec<(&Transaction, &SymbolTable)> =
-            parsed_data.iter().map(|(tr, st)| (tr, st)).collect();
-
-        let todos = vec![(String::from("count_up"), vec![bv(10, 64)])];
-
-        let sim = patronus::sim::Interpreter::new(&ctx, &sys);
-        let mut scheduler = Scheduler::new(
-            transactions_and_symbols.clone(),
-            todos.clone(),
-            &ctx,
-            &sys,
-            sim,
-            handler,
-        );
-        let results = scheduler.execute_todos();
-        assert_ok(&results[0]);
-    }
-
-    #[test]
     fn test_scheduler_aes128() {
         let handler = &mut DiagnosticHandler::default();
         let (parsed_data, ctx, sys) = setup_test_environment(
