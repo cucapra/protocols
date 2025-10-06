a7c55e0102a595e4eae903860713bbf0812151b6
Ernest Ng <eyn5@cornell.edu>
2025-10-06 16:35:43 -0400

    Copyright info

--- protocols/src/scheduler.rs
+++ protocols/src/scheduler.rs
@@ -3,6 +3,7 @@
 // author: Nikil Shyamunder <nvs26@cornell.edu>
 // author: Kevin Laeufer <laeufer@cornell.edu>
 // author: Francis Pham <fdp25@cornell.edu>
+// author: Ernest Ng <eyn5@cornell.edu>
 
 use baa::BitVecValue;
 use log::info;
