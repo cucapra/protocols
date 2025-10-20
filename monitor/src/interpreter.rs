8283f5f2d6ec613595cd9d7402ad197b3713c122
Ernest Ng <eyn5@cornell.edu>
2025-10-20 13:54:26 -0400

    Handle unseen symbols on RHS of assignments

--- monitor/src/interpreter.rs
+++ monitor/src/interpreter.rs
@@ -350,7 +350,28 @@ impl Interpreter {
             }
             Ok(ExprValue::DontCare) => Ok(()),
             Err(ExecutionError::Symbol(SymbolError::NotFound { .. })) => {
-                todo!()
+                let expr = &self.transaction[expr_id];
+                if let Expr::Sym(symbol_id) = expr {
+                    let symbol_name = self.symbol_table[symbol_id].full_name(&self.symbol_table);
+                    info!("RHS of assignment is a symbol {} that is not in the args_mapping, adding it...", symbol_name);
+                    if let Ok(trace_value) = ctx.trace.get(ctx.instance_id, *symbol_id) {
+                        self.args_mapping.insert(*symbol_id, trace_value.clone());
+                        info!(
+                            "Updated args_mapping to map {} |-> {:?}",
+                            symbol_name, trace_value
+                        );
+                        Ok(())
+                    } else {
+                        Err(ExecutionError::symbol_not_found(
+                            *symbol_id,
+                            symbol_name,
+                            "trace".to_string(),
+                            *expr_id,
+                        ))
+                    }
+                } else {
+                    todo!()
+                }
             }
             Err(_) => todo!(),
         }
