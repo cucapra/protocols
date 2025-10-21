e38871a0e936232045c28274c3e5a2ad60d08ed8
Kevin Läufer <laeufer@cornell.edu>
2025-10-21 10:16:31 -0400

    add ALU test

---
+++ protocols/tests/alus/alu_d1.v
@@ -0,0 +1,17 @@
+module alu_d1 (
+    input              clk, 
+    input       [31:0] a,
+    input       [31:0] b,
+    input       [1:0] op,
+    output reg  [31:0] s
+);  
+
+    always @(posedge clk) begin
+        case(op)
+          2'b00: s <= a + b;
+          2'b01: s <= a - b;
+          2'b10: s <= a & b;
+          2'b11: s <= a | b;
+        endcase
+    end
+endmodule
