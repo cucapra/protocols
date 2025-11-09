2100b9c1a7396e605ec959919276381fb7708351
Ernest Ng <eyn5@cornell.edu>
2025-11-09 17:47:41 -0500

    Implement well-formedness check for assignments

--- protocols/src/static_checks.rs
+++ protocols/src/static_checks.rs
@@ -1,10 +1,18 @@
 use crate::{
     diagnostic::{DiagnosticHandler, Level},
-    ir::{Dir, Expr, ExprId, SymbolId, SymbolTable, Transaction, Type},
+    ir::{BinOp, Dir, Expr, ExprId, StmtId, SymbolId, SymbolTable, Transaction, Type},
     serialize::serialize_expr,
 };
 use anyhow::anyhow;
 use itertools::Itertools;
+
+/// Enum representing a location in the AST that
+/// is either an expression (identified by its `ExprId`)
+/// or a statement (identified by its `StmtId`)
+pub enum LocationId {
+    Expr(ExprId),
+    Stmt(StmtId),
+}
 
 /// Enum representing *language features* for which static well-formedness
 /// checks need to be performed
@@ -31,8 +39,9 @@ impl std::fmt::Display for LangFeature {
 
 /// Checks if the identifier corresponding to `symbol_id` is a DUT port /
 /// function parameter with the specified `direction`.
-/// - `expr_id` is the `ExprId` of the expression to which `symbol_id` belongs
-///   (used for more accurate error message locations)
+/// - `location_id` is the `LocationId` (either an `ExprId` or a `StmtId`)
+///   to which `symbol_id` belongs
+///   (this is used for more accurate error message locations)
 /// - The associated `Transaction`, `SymbolTable` & `DiagnosticHandler`
 ///   are also expected as inputs (for error message purposes)
 /// - `language_feature` is a `LangFeature` enum that describes the corresponding
@@ -41,23 +50,34 @@ impl std::fmt::Display for LangFeature {
 pub fn check_if_symbol_is_dut_port(
     symbol_id: SymbolId,
     direction: Dir,
-    expr_id: ExprId,
+    location_id: LocationId,
     tr: &Transaction,
     symbol_table: &SymbolTable,
     handler: &mut DiagnosticHandler,
     lang_feature: LangFeature,
 ) -> anyhow::Result<()> {
+    // Helper closure that figures out whether to call `emit_diagnostic_expr`
+    // or `emit_diagnostic_stmt` based on `location_id`
+    let emit_diagnostic = |handler: &mut DiagnosticHandler, error_msg: &str| match &location_id {
+        LocationId::Expr(expr_id) => {
+            handler.emit_diagnostic_expr(tr, expr_id, error_msg, Level::Error);
+        }
+        LocationId::Stmt(stmt_id) => {
+            handler.emit_diagnostic_stmt(tr, stmt_id, error_msg, Level::Error);
+        }
+    };
+
     // Fully-qualify the name of the identifier
     let symbol_full_name = symbol_table.full_name_from_symbol_id(&symbol_id);
 
     match (tr.type_param, symbol_table[symbol_id].parent()) {
         (None, _) => {
             let error_msg = format!(
-                "Expected {} to be an {}put parameter / {}put field of a struct, 
+                "Expected {} to be an {}put parameter / {}put field of a struct,
                             but the function {} is not parameterized by any structs",
                 symbol_full_name, direction, direction, tr.name
             );
-            handler.emit_diagnostic_expr(tr, &expr_id, &error_msg, Level::Error);
+            emit_diagnostic(handler, &error_msg);
             Err(anyhow!(error_msg))
         }
         (Some(_), None) => {
@@ -72,7 +92,7 @@ pub fn check_if_symbol_is_dut_port(
 
             let error_msg_prefix = if has_opposite_direction {
                 format!(
-                    "{} is an {}put parameter of {}, which is illegal 
+                    "{} is an {}put parameter of {}, which is illegal
                     (a {} parameter is expected in {})",
                     symbol_full_name, !direction, tr.name, direction, lang_feature
                 )
@@ -80,13 +100,13 @@ pub fn check_if_symbol_is_dut_port(
                 format!("Unrecognized identifier {}", symbol_full_name)
             };
             let error_msg = format!(
-                            "{error_msg_prefix} 
+                            "{error_msg_prefix}
                             (Only {}put parameters / {}put fields of structs are allowed to appear in {}",
                             direction,
                             direction,
                             lang_feature
                         );
-            handler.emit_diagnostic_expr(tr, &expr_id, &error_msg, Level::Error);
+            emit_diagnostic(handler, &error_msg);
             Err(anyhow!(error_msg))
         }
         (Some(struct_id), Some(parent_symbol_id)) => {
@@ -113,24 +133,20 @@ pub fn check_if_symbol_is_dut_port(
                     Ok(())
                 } else {
                     let error_msg = format!(
-                        "`{}` is not an {}put field of the struct `{}` 
+                        "`{}` is not an {}put field of the struct `{}`
                         (Only {}put fields are allowed to appear in {})",
                         symbol_full_name, direction, struct_name, direction, lang_feature
                     );
-                    handler.emit_diagnostic_expr(tr, &expr_id, &error_msg, Level::Error);
+                    emit_diagnostic(handler, &error_msg);
                     Err(anyhow!(error_msg))
                 }
             } else {
                 let parent_name = symbol_table[parent_symbol_id].name();
                 let error_msg = format!(
                     "Expected {} to be an output field of struct {}({}) but got {}({}) as the parent struct instead",
-                    serialize_expr(tr, symbol_table, &expr_id),
-                    struct_name,
-                    struct_id,
-                    parent_name,
-                    parent_symbol_id
+                    symbol_full_name, struct_name, struct_id, parent_name, parent_symbol_id
                 );
-                handler.emit_diagnostic_expr(tr, &expr_id, &error_msg, Level::Error);
+                emit_diagnostic(handler, &error_msg);
                 Err(anyhow!(error_msg))
             }
         }
@@ -159,7 +175,7 @@ pub fn check_condition_wf(
                 check_if_symbol_is_dut_port(
                     *symbol_id,
                     Dir::Out,
-                    *expr_id,
+                    LocationId::Expr(*expr_id),
                     tr,
                     symbol_table,
                     handler,
@@ -177,9 +193,8 @@ pub fn check_condition_wf(
             check_condition_wf(expr_id1, tr, symbol_table, handler)?;
             check_condition_wf(expr_id2, tr, symbol_table, handler)
         }
-        Expr::Unary(_, inner_expr) => check_condition_wf(inner_expr, tr, symbol_table, handler),
-        Expr::Slice(sliced_expr, _, _) => {
-            check_condition_wf(sliced_expr, tr, symbol_table, handler)
+        Expr::Unary(_, inner_expr) | Expr::Slice(inner_expr, _, _) => {
+            check_condition_wf(inner_expr, tr, symbol_table, handler)
         }
     }
 }
@@ -201,7 +216,7 @@ pub fn check_assertion_lhs_wf(
             check_if_symbol_is_dut_port(
                 *lhs_symbol_id,
                 Dir::Out,
-                *lhs_expr_id,
+                LocationId::Expr(*lhs_expr_id),
                 tr,
                 st,
                 handler,
@@ -242,7 +257,7 @@ pub fn check_assertion_rhs_wf(
                 check_if_symbol_is_dut_port(
                     *symbol_id,
                     Dir::Out,
-                    *rhs_expr_id,
+                    LocationId::Expr(*rhs_expr_id),
                     tr,
                     symbol_table,
                     handler,
@@ -308,5 +323,102 @@ pub fn check_assertion_wf(
     }
 }
 
-// TODO: refactor the logic for checking assignment WF-ness from `monitor/src/interpreter.rs`
-// into a helper function `check_assignment_well_formedness`
+/// Recursively checks whether the RHS of an assignment
+/// (identified by `rhs_expr_id`) is well-formed (WF).
+/// - The `dont_cares_allowed` argument is used to determine whether
+///   `DontCare` is allowed to appear during the particular call to this function.
+///   This arg is set to `true` in the top-level call to this function since we
+///   allow `DontCare` to appear as the only expression on the RHS of an
+///   assignment, but we set to `false` during recursive calls since we don't
+///   allow `DontCare` to appear as an argument to unary/binary/slice operators.
+pub fn check_assignment_rhs_wf(
+    rhs_expr_id: &ExprId,
+    dont_cares_allowed: bool,
+    tr: &Transaction,
+    symbol_table: &SymbolTable,
+    handler: &mut DiagnosticHandler,
+) -> anyhow::Result<()> {
+    let rhs_expr = &tr[rhs_expr_id];
+    match rhs_expr {
+        Expr::Const(_) => Ok(()),
+        Expr::DontCare if dont_cares_allowed => Ok(()),
+        Expr::DontCare => {
+            // We need this case to prevent DontCares from appearing as a sub-expression on the RHS
+            // (DontCare is only allowed as the *only* expression on the RHS of an assignment)
+            let error_msg = "DontCare cannot appear as a sub-expression on the RHS of an assignment. 
+                        If you need a DontCare on the RHS of assignment, 
+                        change the statement to `<lhs> := X` where `DontCare` is the *only* expression on the RHS.";
+            handler.emit_diagnostic_expr(tr, rhs_expr_id, error_msg, Level::Error);
+            Err(anyhow!(error_msg))
+        }
+        Expr::Sym(symbol_id) => {
+            // Check if the identifier is an output parameter of the function
+            if tr.is_param_with_correct_direction(*symbol_id, Dir::Out) {
+                Ok(())
+            } else {
+                // Check if the identifier is a DUT output port
+                check_if_symbol_is_dut_port(
+                    *symbol_id,
+                    Dir::Out,
+                    LocationId::Expr(*rhs_expr_id),
+                    tr,
+                    symbol_table,
+                    handler,
+                    LangFeature::Conditionals,
+                )
+            }
+        }
+        Expr::Binary(BinOp::Concat, expr_id1, expr_id2) => {
+            // Recursively check whether the two operands are well-formed
+            // (Note that we disallow `DontCare`s to appear as arguments
+            // to the concatenation)
+            check_assignment_rhs_wf(expr_id1, false, tr, symbol_table, handler)?;
+            check_assignment_rhs_wf(expr_id2, false, tr, symbol_table, handler)
+        }
+        Expr::Binary(_, _, _) => {
+            // Other binary operators (e.g. the `==` comparison operator)
+            // are not allowed on the RHS of assignments
+            let error_msg = format!(
+                "Unsupported binary operation {} on the RHS of an assignment",
+                serialize_expr(tr, symbol_table, rhs_expr_id)
+            );
+            handler.emit_diagnostic_expr(tr, rhs_expr_id, &error_msg, Level::Error);
+            Err(anyhow!(error_msg))
+        }
+        Expr::Unary(_, inner_expr) | Expr::Slice(inner_expr, _, _) => {
+            // Check if the inner expression is well-formed
+            // (Note: we do not allow the inner expression to be `DontCare`)
+            check_assignment_rhs_wf(inner_expr, false, tr, symbol_table, handler)
+        }
+    }
+}
+
+/// Checks whether an assignment is well-formed.
+/// - `lhs_symbol_id` & `rhs_expr_id` correspond to the LHS & RHS of the
+///   assignment respectively
+/// - `stmt_id` is the `StmtId` of the entire assignemnt statemnet
+///   (used to track the location of the statement in error messages)
+/// - The associated `Transaction`, `SymbolTable` & `DiagnosticHandler`
+///   are also expected as inputs (for error message purposes)
+pub fn check_assignment_wf(
+    lhs_symbol_id: &SymbolId,
+    rhs_expr_id: &ExprId,
+    stmt_id: &StmtId,
+    tr: &Transaction,
+    symbol_table: &SymbolTable,
+    handler: &mut DiagnosticHandler,
+) -> anyhow::Result<()> {
+    // Check if the LHS is a DUT input port
+    check_if_symbol_is_dut_port(
+        *lhs_symbol_id,
+        Dir::In,
+        LocationId::Stmt(*stmt_id),
+        tr,
+        symbol_table,
+        handler,
+        LangFeature::Assignments,
+    )?;
+
+    // Check if the RHS is well-formed
+    check_assignment_rhs_wf(rhs_expr_id, true, tr, symbol_table, handler)
+}
