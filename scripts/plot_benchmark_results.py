0b274551fc59dfe5e647709986ce996d89932965
Ernest Ng <eyn5@cornell.edu>
2025-12-12 14:47:36 -0500

    Update CSVs and axis labels

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -5,7 +5,6 @@ Plots the data in `INPUT_CSV` as a scatter plot, saving it in `OUTPUT_PNG`
 
 import csv
 import math
-import matplotlib
 import matplotlib.pyplot as plt
 
 INPUT_CSV = "benchmark_results/benchmark_results.csv"
@@ -22,13 +21,23 @@ def main():
             # Read pre-computed throughput (steps per second)
             steps_per_sec.append(float(row["steps_per_sec"]))
 
+    # Determine min/max values on the y-axis, round them to nearest multiple of 10
+    min_steps_per_sec = min(steps_per_sec)
+    max_steps_per_sec = max(steps_per_sec)
+    ymin = math.floor(min_steps_per_sec / 10.0) * 10.0
+    ymax = math.ceil(max_steps_per_sec / 10.0) * 10.0
+
+    # Create ticks for every 200 units on the y-axis
+    yticks = list(range(int(ymin), int(ymax) + 1, 200))
+
     plt.figure(figsize=(12, 8))
     plt.scatter(names, steps_per_sec)
 
     plt.title("Monitor throughput on benchmarks (higher is better)")
     plt.xlabel("Test File")
-    plt.ylabel("`Step`s (clock cycles) per second")
+    plt.ylabel("Mean no. of Steps (clock cycles) per second")
     plt.xticks(rotation=45, ha="right")
+    plt.yticks(yticks)
 
     plt.grid(zorder=1, linestyle="--", alpha=0.6)
     plt.tight_layout()
