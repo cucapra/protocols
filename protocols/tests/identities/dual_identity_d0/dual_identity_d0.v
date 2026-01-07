3d5f383f4e0c99070fa6a466447b01b5371721e3
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2026-01-07 15:57:42 -0800

    add a good test case

---
+++ protocols/tests/identities/dual_identity_d0/dual_identity_d0.v
@@ -0,0 +1,10 @@
+module identity_d0 (
+    input              clk,
+    input       a,
+    input       b,
+    output      s1,
+    output      s2
+);
+    assign s1 = a;
+    assign s2 = b;
+endmodule
