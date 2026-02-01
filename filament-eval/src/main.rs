e05435e2d7ba9d894472f8cb9514e226745c51c4
Ernest Ng <eyn5@cornell.edu>
2026-02-01 16:44:47 -0500

    Extract event variables

--- filament-eval/src/main.rs
+++ filament-eval/src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
+use serde::Deserialize;
 use serde_json::Value;
-use std::path::Path;
+use std::{fmt, path::Path};
 
+/// CLI arguments for the Filament to Protocols compiler
 #[derive(Parser)]
 #[command(
     version,
@@ -14,6 +16,46 @@ struct Cli {
     json: String,
 }
 
+/// A Filament event variable
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+struct Event {
+    /// The name of the event
+    name: String,
+
+    /// The delay associated with the event
+    delay: u32,
+}
+
+impl fmt::Display for Event {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "<{}: {}>", self.name, self.delay)
+    }
+}
+
+/// Extracts all the `Event`s from the Filament interface JSON
+fn get_events(json: &Value) -> Vec<Event> {
+    let interface_ports = json["interfaces"]
+        .as_array()
+        .expect("Expected `interfaces` to be a JSON array");
+    let mut events = vec![];
+    for port in interface_ports {
+        let event_name = port["event"]
+            .as_str()
+            .expect("Expected `event` to be a string")
+            .to_string();
+        let delay = port["delay"]
+            .as_i64()
+            .expect("Expected `delay` to be an integer") as u32;
+        let event = Event {
+            name: event_name,
+            delay,
+        };
+        events.push(event);
+    }
+    events
+}
+
+/// Main entry point for the executable
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     let filepath_str = cli.json;
@@ -21,8 +63,10 @@ fn main() -> anyhow::Result<()> {
 
     if let Some("json") = filepath.extension().and_then(|s| s.to_str()) {
         let file_contents = std::fs::read_to_string(filepath)?;
-        let _json: Value = serde_json::from_str(&file_contents)
+        let json: Value = serde_json::from_str(&file_contents)
             .unwrap_or_else(|_| panic!("Unable to read from JSON file {filepath_str}"));
+        let events = get_events(&json);
+        println!("events: {:?}", events);
         Ok(())
     } else {
         panic!("Invalid extension for file {filepath_str}, expected JSON file");
