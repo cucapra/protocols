3397410b8d43aec18142f97c1608692e7ebcc910
Kevin Läufer <laeufer@cornell.edu>
2026-02-18 09:53:23 -0500

    add python bindings project

---
+++ python/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2026 Cornell University
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@cornell.edu>
+
+use pyo3::prelude::*;
+
+#[pymodule]
+#[pyo3(name = "protocols")]
+fn protocols(_py: Python<'_>, m: &pyo3::Bound<'_, pyo3::types::PyModule>) -> PyResult<()> {
+  Ok(())
+}
