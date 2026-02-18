261b2e24b2eb1560c3620232805522aca3770875
Kevin Läufer <laeufer@cornell.edu>
2026-02-18 11:53:58 -0500

    python: make sure we can import the library

---
+++ python/tests/test_lib.py
@@ -0,0 +1,14 @@
+# Copyright 2026 Cornell University
+# released under BSD 3-Clause License
+# author: Kevin Laeufer <laeufer@cornell.edu>
+
+import pathlib
+import pytest
+from protocols import *
+
+
+repo_root = (pathlib.Path(__file__) / '..' / '..' / '..').resolve()
+
+
+def test_something():
+    pass
