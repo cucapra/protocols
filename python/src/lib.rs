4db7479e0965e03b656b0c8e66461575d5e6062b
Kevin Läufer <laeufer@cornell.edu>
2026-02-18 12:17:32 -0500

    python: expose native functions through python wrapper

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,11 +1,17 @@
 // Copyright 2026 Cornell University
-// released under BSD 3-Clause License
+// released under MIT License
 // author: Kevin Laeufer <laeufer@cornell.edu>
 
 use pyo3::prelude::*;
 
+#[pyfunction]
+pub fn proto_test_fn() -> u64 {
+    42
+}
+
 #[pymodule]
-#[pyo3(name = "protocols")]
+#[pyo3(name = "pyprotocols")]
 fn protocols(_py: Python<'_>, m: &pyo3::Bound<'_, pyo3::types::PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(proto_test_fn, m)?)?;
     Ok(())
 }
