6bfdfd05137b19e4f35e760de5319ca73490af31
Kevin Läufer <laeufer@cornell.edu>
2026-03-17 11:09:10 -0400

    bi: do not emit prefix traces

--- bi/src/proto_trace.rs
+++ bi/src/proto_trace.rs
@@ -81,6 +81,26 @@ impl Traces {
         let mut tails: Vec<_> = self.tails.iter().cloned().flatten().collect();
         tails.sort();
         tails.dedup();
+
+        // We visit all traces and count the usage in order to ensure that no trace
+        // returned is a prefix of another returned trace.
+        let mut uses = vec![0; self.entries.len()];
+        let mut todo = tails.clone();
+        while let Some(entry_id) = todo.pop() {
+            uses[entry_id as usize] += 1;
+            if let Some(prev) = self.entries[entry_id as usize].prev {
+                todo.push(prev);
+            }
+        }
+
+        // filter out any tails that are used more than once since that implies that they are
+        // the tail of a prefix trace
+        debug_assert!(
+            !tails.iter().any(|&t| uses[t as usize] == 0),
+            "all tails should be used at least once"
+        );
+        tails.retain(|&t| uses[t as usize] == 1);
+
         tails
             .into_iter()
             .map(|t| self.get_trace_from_tail(t))
