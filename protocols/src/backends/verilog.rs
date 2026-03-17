a2058ee12fc8110078c9e2dc917e1e0ef718420b
Kevin Läufer <laeufer@cornell.edu>
2026-03-16 21:29:36 -0400

    fmt

--- protocols/src/backends/verilog.rs
+++ protocols/src/backends/verilog.rs
@@ -395,10 +395,7 @@ pub mod tests {
 
     #[test]
     fn alu_d1_to_verilog() {
-        let protos = frontend(
-            "tests/alus/alu_d1.prot",
-            &mut DiagnosticHandler::default(),
-        );
+        let protos = frontend("tests/alus/alu_d1.prot", &mut DiagnosticHandler::default());
         let tx = [
             (
                 "add".into(),
