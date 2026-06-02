8080380b3d7d61bafb3f7a9387d62331bf83e60a
Nikil Shyamsunder <nvs26@cornell.edu>
2026-06-01 23:14:49 -0400

    add lowering
    switch internal struct fields to private, use constructors and accessorts

--- protocols/src/ir/mod.rs
+++ protocols/src/ir/mod.rs
@@ -1,8 +1,7 @@
-mod ir;
-mod lowering;
+pub mod lowering;
+mod proto_graph;
 
 // TODO: add a function to transform AST to IR
 // pub fn frontend(
 //     ast : ast::Protocol
 // ) -> ir::Protocol
-
--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -8,8 +8,8 @@
 pub mod backends;
 pub mod errors;
 pub mod frontend;
-pub mod ir;
 pub mod interpreter;
+pub mod ir;
 pub mod scheduler;
 pub mod setup;
 pub mod transactions;
