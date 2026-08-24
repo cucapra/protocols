8a62a7b29caf13e93daa7ff75c3cae61411e9c18
Kevin Läufer <laeufer@cornell.edu>
2026-08-24 09:54:41 -0400

    bi: simplify stream termination logic

--- bi/src/main.rs
+++ bi/src/main.rs
@@ -261,18 +261,16 @@ fn run_bis(
 ) -> Result<StepToTime, String> {
     trace.stream_steps(|_step_id, values| {
         // step all backwards interpreters that have not failed
-        let mut exec_count = 0;
+        let mut r = CallbackResult::Stop;
         for bi in bis.iter_mut() {
             if !bi.has_failed() {
                 let _ = bi.exec_step(values.clone());
-                exec_count += 1;
+                if !bi.has_failed() {
+                    r = CallbackResult::Continue;
+                }
             }
         }
-        if exec_count == 0 {
-            CallbackResult::Stop
-        } else {
-            CallbackResult::Continue
-        }
+        r
     })?;
 
     for bi in bis.iter_mut() {
