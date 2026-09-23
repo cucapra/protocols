e64d60e46577e2c7c7d0df0d7120e3c271e28b9f
Kevin Läufer <laeufer@cornell.edu>
2026-09-23 12:05:20 -0400

    functional model for multiplier

--- functional-models/fun.py
+++ functional-models/fun.py
@@ -1,11 +1,14 @@
+import json
 from dataclasses import dataclass, field
 from pypatronus import TransitionSystem, BitVec
+
 
 @dataclass
 class FunctionalModel:
     name: str
     transactions: list = field(default_factory=list)
     states: list = field(default_factory=list)
+
 
 @dataclass
 class Transaction:
@@ -15,22 +18,42 @@ class Transaction:
     state_updates: list = field(default_factory=list)
 
 
-def verify_model(m :FunctionalModel):
+def verify_model(m: FunctionalModel):
+    assert len(m.states) == 0, "TODO: deal with states"
     for transaction in m.transactions:
         assert len(transaction.state_updates) == len(m.states)
-        for output in transaction.outputs:
-            # TODO: check symbols
-            pass
+        allowed_symbols = set(m.states) | set(transaction.inputs)
 
-def serialize(m :FunctionalModel):
+        for out_name, out_expr in transaction.outputs:
+            unallowed = out_expr.symbols() - allowed_symbols
+            assert len(unallowed) == 0, (
+                f"Output {out_name} uses symbols that are neither inputs nor state: {unallowed}"
+            )
+
+
+def serialize(m: FunctionalModel, filename):
+    assert len(m.states) == 0, "TODO: deal with states"
     verify_model(m)
-
-    inputs = []
-    enables = []
-    for t in m.transactions:
-        inputs.append(BitVec(f"{t.name}_enable", 1))
-        enables.append(inputs[-1])
-        for inp in t.inputs:
-            inputs.append(BitVec(f"{t.name}_in_{inp.name}", 1))
-
     sys = TransitionSystem(name=m.name)
+    for t in m.transactions:
+        commit_signal = BitVec(f"{t.name}_commit", 1)
+        sys.add_input(commit_signal)
+        input_map = {}
+        for inp in t.inputs:
+            renamed = BitVec(f"{t.name}_in_{inp.name()}", inp.width())
+            input_map[inp] = renamed
+            sys.add_input(renamed)
+        for out_name, out_expr in t.outputs:
+            out_expr = out_expr.replace(input_map)
+            sys.add_output(f"{t.name}_out_{out_name}", out_expr)
+
+    transactions = [{"name": t.name} for t in m.transactions]
+    info = {
+        "name": m.name,
+        "transactions": transactions,
+        "states": [s.name for s in m.states],
+        "sys": sys.to_btor2_str(),
+    }
+
+    with open(filename, "w") as f:
+        json.dump(info, f)
--- functional-models/main.py
+++ functional-models/main.py
@@ -11,22 +11,32 @@ def picorv32_pcpi_mul():
     https://github.com/ekiwi/paso/blob/ad2bf83f420ca704ff0e76e7a583791a0e80a545/benchmarks/src/benchmarks/picorv32/PicoRV32Spec.scala#L8
     """
     m = FunctionalModel(name="picorv32_pcpi_mul")
-    rs1, rs2 = BitVec('rs1_data', 32),BitVec('rs2_data', 32)
+    rs1, rs2 = BitVec("rs1_data", 32), BitVec("rs2_data", 32)
     m.transactions = [
         Transaction("pcpi_mul", [rs1, rs2], [("rd_data", rs1 * rs2)]),
-        Transaction("pcpi_mulh", [rs1, rs2], [("rd_data", Slice(63, 32, SignExt(32, rs1) * SignExt(32, rs2)))]),
-        Transaction("pcpi_mulhu", [rs1, rs2], [("rd_data", Slice(63, 32, ZeroExt(32, rs1) * ZeroExt(32, rs2)))]),
-        Transaction("pcpi_mulhsu", [rs1, rs2], [("rd_data", Slice(63, 32, SignExt(32, rs1) * ZeroExt(32, rs2)))]),
+        Transaction(
+            "pcpi_mulh",
+            [rs1, rs2],
+            [("rd_data", Slice(63, 32, SignExt(32, rs1) * SignExt(32, rs2)))],
+        ),
+        Transaction(
+            "pcpi_mulhu",
+            [rs1, rs2],
+            [("rd_data", Slice(63, 32, ZeroExt(32, rs1) * ZeroExt(32, rs2)))],
+        ),
+        Transaction(
+            "pcpi_mulhsu",
+            [rs1, rs2],
+            [("rd_data", Slice(63, 32, SignExt(32, rs1) * ZeroExt(32, rs2)))],
+        ),
     ]
     return m
-
 
 
 def main():
     m = picorv32_pcpi_mul()
     print(m)
-    serialize(m)
-
+    serialize(m, "picorv32_pcpi_mul.json")
 
 
 if __name__ == "__main__":
